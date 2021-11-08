32befc3680b061e33d3985de02a75e4dd88e3d9d
yunko006 <peyrachethomas@gmail.com>
2021-11-08 14:02:48 +0100

    Get data from the api and write them to excel

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 import os
 from dotenv import load_dotenv
 
+import requests
+import json
+import pandas as pd
+
 load_dotenv()
 
 MINER_KEY = os.getenv("MINER_KEY")
 
-print(MINER_KEY)
+response_API = requests.get(
+    f"https://api.ethermine.org/miner/:{MINER_KEY}/dashboard")
+
+data = response_API.text
+
+parse_json = json.loads(data)
+
+statistics = parse_json['data']['statistics']
+
+df = pd.DataFrame.from_dict(statistics)
+
+print(df)
+
+df.to_excel('statistics.xlsx')
